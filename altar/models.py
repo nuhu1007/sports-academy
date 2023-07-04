dfec417dddd2abcc37abff580bdbeab58a6a875e
Abdul <nuhu@mpc.com.ng>
2023-07-04 12:18:00 +0300

    created player model

--- altar/models.py
+++ altar/models.py
@@ -25,3 +25,23 @@ class User(AbstractUser):
 
     def __str__(self):
         return str(self.email)
+    
+
+class Player(models.Model):
+    full_name = models.CharField(max_length=150, blank=False, null=False)
+    date_of_birth = models.CharField(max_length=100, blank=False, null=False)
+    home_address = models.CharField(max_length=200, blank=False, null=False)
+    school_attended = models.CharField(max_length=200, blank=False, null=False)
+    player_position = models.CharField(max_length=100, null=True, blank=True)
+    medical_condition = models.BooleanField(default=False)
+    parent_full_name = models.CharField(max_length=150, blank=False, null=False)
+    parent_phone_number = models.CharField(max_length=20, blank=False, null=False)
+    emergency_contact = models.CharField(max_length=20, blank=False, null=False)
+    email_address = models.EmailField(blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_joined']
+
+    def __str__(self):
+        return str(self.full_name)
