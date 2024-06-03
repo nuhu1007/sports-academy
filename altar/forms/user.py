e21aaeddb6953863a7f37fa3d415b8e218aa0fa4
Abdul <nuhu@mpc.com.ng>
2024-06-03 10:40:23 +0300

    edited login form

--- altar/forms/user.py
+++ altar/forms/user.py
@@ -20,7 +20,3 @@ class RegistrationForm(UserCreationForm):
 class LoginForm(forms.Form):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder':'Enter your email', 'class':'custom-email-input form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Enter your password', 'class':'prompt srch_explore form-control'}))
-
-    # class Meta:
-    #     model = User
-    #     fields = ['email', 'password']
